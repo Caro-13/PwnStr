0399d910f2c8823d82d5a620f076f6a7d17feb49
wgoni <raphael.gonin@hes-so.ch>
2025-12-11 16:31:54 +0100

    Task distribution

--- Bots/PawnStrBot.py
+++ Bots/PawnStrBot.py
@@ -1,8 +1,5 @@
-from decorator import append
-boardLength = 8
-from parso.python.tree import String
+from PwnStr import *
 
-#
 #   Example function to be implemented for
 #       Single important function is next_best
 #           color: a single character str indicating the color represented by this bot ('w' for white)
@@ -22,6 +19,8 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
             if board[x,y][-1] != color:
                 continue
             print(board[x,y].string(), (x,y))
+            pos = (x, y)
+            checkDiagonal(pos, board, color)
 
         print("\n")
 
@@ -29,54 +28,3 @@ def chess_bot(player_sequence, board, time_budget, **kwargs):
 
 #   Example how to register the function
 register_chess_bot("PwnStr", chess_bot)
-
-def checkDiagonal(pos, board, color):
-    x = pos[0]
-    y = pos[1]
-    possibleMoves = []
-
-    rightBottum = y if y < x else x
-    rightUp = y if y < boardLength-x else boardLength-x
-
-    leftBottum = boardLength-y if boardLength-y < x else x
-    leftUp =  boardLength-y if boardLength-y < boardLength-x else boardLength-x
-
-    def goDiagonal(horiz,vert,borne,step):
-        for i in range(x, borne,step):
-            nextPos = (x + i*vert, y + i*horiz)
-            if nextPos == "":
-                possibleMoves.append((pos, nextPos))
-
-            if board[nextPos][-1] != color:
-                possibleMoves.append((pos, nextPos))
-                break
-
-            if board[nextPos][-1] == color:
-                break
-
-    goDiagonal(-1,1,rightUp,1)
-    goDiagonal(-1,-1,rightBottum,-1)
-    goDiagonal(1,1,leftUp,1)
-    goDiagonal(1,-1,leftBottum,-1)
-
-
-    return possibleMoves
-
-
-def chechHorizVerti(pos, board, color):
-    possibleMoves = []
-    return possibleMoves
-
-def checkL (pos, board, color):
-    possibleMoves = []
-    return possibleMoves
-
-def checkCarre(pos, board, color):
-    possibleMoves = []
-    return possibleMoves
-
-def checkPawn(pos, board, color):
-    possibleMoves = []
-    return possibleMoves
-
-
--- Bots/PwnStr.py
+++ Bots/PwnStr.py
@@ -1,40 +1,50 @@
+# CONST
+boardLength = 8
 
-def check_moves(pos, board, color):
+
+def checkMoves(pos, board, color):
     possibleMoves = []
     p = board[pos][0]
-    match p:
-        case "p":
-            checkPawn(pos, board, color)
-        case "n":
-            checkL(pos, board, color)
-        case "b":
-            checkDiagonal(pos, board, color)
-        case "r":
-            checkHorizVerti(pos, board, color)
-        case "q":
-            checkHorizVerti(pos, board, color)
-            checkDiagonal(pos, board, color)
-        case "k":
-            checkCarre(pos, board, color)
+    c = board[pos][1]
+    for x in range(board.shape[0] - 1):
+        for y in range(board.shape[1]):
+            if c == color:
+                match p:
+                    case "p":
+                        possibleMoves.extend(checkPawn(pos, board, color))
+                    case "n":
+                        possibleMoves.extend(checkL(pos, board, color))
+                    case "b":
+                        possibleMoves.extend(checkDiagonal(pos, board, color))
+                    case "r":
+                        possibleMoves.extend(checkHorizVerti(pos, board, color))
+                    case "q":
+                        possibleMoves.extend(checkHorizVerti(pos, board, color))
+                        possibleMoves.extend(checkDiagonal(pos, board, color))
+                    case "k":
+                        possibleMoves.extend(checkCarre(pos, board, color))
     return possibleMoves
 
-def check_value(p):
+
+def checkValue(p):
     match p:
         case "p":
-            v = 1
+            return 1
         case "n":
-            v = 3
+            return 3
         case "b":
-            v = 3
+            return 3
         case "r":
-            v = 5
+            return 5
         case "q":
-            v = 9
+            return 9
         case "k":
-            v = 90
-    return v
+            return 90
+        case _:
+            return 0
 
-def print_board(board):
+
+def printBoard(board):
     piece = ""
     print("\n")
     for x in range(board.shape[0]):
@@ -42,7 +52,62 @@ def print_board(board):
             if board[x, y] == '':
                 piece += "' ' "
             else:
-                 piece += f"{board[x, y].string()}  "
+                piece += f"{board[x, y].string()}  "
         print(f"{piece}\n")
         piece = ""
     print("\n")
+
+# Caro
+def checkDiagonal(pos, board, color):
+    x = pos[0]
+    y = pos[1]
+    p = board(pos)[0]
+    possibleMoves = []
+
+    rightBottum = y if y < x else x
+    rightUp = y if y < boardLength - x else boardLength - x
+
+    leftBottum = boardLength - y if boardLength - y < x else x
+    leftUp = boardLength - y if boardLength - y < boardLength - x else boardLength - x
+
+    def goDiagonal(horiz, vert, borne, step):
+        for i in range(x, borne, step):
+            nextPos = (x + i * vert, y + i * horiz)
+            if nextPos == "":
+                possibleMoves.append((pos, nextPos, checkValue(p)))
+
+            if board[nextPos][-1] != color:
+                possibleMoves.append((pos, nextPos, checkValue(p)))
+                break
+
+            if board[nextPos][-1] == color:
+                break
+
+    goDiagonal(-1, 1, rightUp, 1)
+    goDiagonal(-1, -1, rightBottum, -1)
+    goDiagonal(1, 1, leftUp, 1)
+    goDiagonal(1, -1, leftBottum, -1)
+
+    print(possibleMoves)
+    return possibleMoves
+
+
+# Raphael
+def checkHorizVerti(pos, board, color):
+    possibleMoves = []
+    return possibleMoves
+
+# Caro
+def checkL(pos, board, color):
+    possibleMoves = []
+    return possibleMoves
+
+# Raphael
+def checkCarre(pos, board, color):
+    possibleMoves = []
+    return possibleMoves
+
+# Caro
+def checkPawn(pos, board, color):
+    possibleMoves = []
+    return possibleMoves
